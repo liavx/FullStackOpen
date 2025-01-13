c733afc1791450f190b2ea0286737d1e870f8f0a
liavx <liav.lavi@gmail.com>
2025-01-13 14:10:48 +0200

    Add solution to 1.2

--- part1/courseinformation/src/App.jsx
+++ part1/courseinformation/src/App.jsx
@@ -1,47 +1,59 @@
 /* eslint-disable react/prop-types */
 const Header = (props) => {
   return (
-    <>{props.course}</>
+    <h1>{props.course}</h1>
   )
 
 }
 
+const Part = (props) =>{
+  return(
+<p>
+  {props.part} {props.exercise}
+</p>
+  )
+}
+
 const Content = (props) =>{
   return (
-    <>{props.part} {props.exercise}</>
+    <>
+    <Part part = {props.parts[0].name} exercise = {props.parts[0].exercise}/>
+    <Part part = {props.parts[1].name} exercise = {props.parts[1].exercise} />
+    <Part part = {props.parts[2].name} exercise = {props.parts[2].exercise} />
+    </>
   )
 
 }
 
 const Total = (props) => {
 return (
-  <p>Number of exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  <p>Number of exercises: {props.parts[0].exercise + props.parts[1].exercise + props.parts[2].exercise}</p>
 )
 }
 
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {name:"Fudamentls of React",
+     exercise:10
+    },
+    {
+      name:"Using props to pass data",
+      exercise:7
+    },
+    {
+      name:"State of component",
+      exercise:14
+    }
+  ]
+
 
   return (
     <>
       <Header course = {course} />
-      <p>
-      <Content exercise={exercises1} part={part1} />
-      </p>
-      <p>
-      <Content exercise={exercises2} part={part2} />
-      </p>
-      <p>
-      <Content exercise={exercises3} part={part3} />
-      </p>
-      <Total  exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
+      <Content parts = {parts} />
+      <Total parts = {parts} />
     </>
   )
 }
