dcb551a4a63f4d5c22780a1e5ba6b47eef589ea9
liavx <liav.lavi@gmail.com>
2025-03-22 17:36:21 +0200

    Solved phonebook database part2

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 const morgan = require('morgan')
 const app = express()
+const Person = require('./models/person.js')
 app.use(express.json())
 app.use(morgan(':method :url :status :response-time ms - :data'));
 app.use(express.static('dist'))
@@ -8,71 +11,53 @@ app.use(express.static('dist'))
 morgan.token('data', function (req) {
     return req.body ? JSON.stringify(req.body) : ''
   })
-let phonebook = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons' , (req,res) =>{
-    res.json(phonebook)
+       Person.find({}).then(persons => {
+        if(persons.length > 0){
+        res.json(persons)
+    }else{
+        console.log("database is empty")
+    }
+        mongoose.connection.close()
+      })
 })
 
 app.get('/api/info' , (req,res) =>{
-    const info = `phonebook has info for ${phonebook.length} people`
+    Person.countDocuments({}).then(count =>{
+    const info = `phonebook has info for ${count} people`
     const date = new Date()
     res.send(info + `<br/>` + date)
+    }).catch(error => res.status(500).json({error:'Error retrieving data'}))
 })
 
 app.get('/api/persons/:id' , (req,res) =>{
-const singlePerson = phonebook.find(person => person.id === req.params.id)
-if(singlePerson){
-    res.json(singlePerson)
-} else{
-    res.statusMessage = "No valid ID";
-    res.status(404).end("NO ID BEEN FOUND")
-}
-
+    Person.findById(req.params.id).then(person =>{
+        if(person){
+            res.json(person)
+        }
+        else{
+            res.statusMessage = "No valid ID";
+            res.status(404).end("NO ID BEEN FOUND")
+        
+        }
+    })
 })
 
 app.post('/api/persons' , (req,res) => {
     const body = req.body
-    console.log(body)
-
     if(!body.name || !body.number) {
         return res.status(400).json({
             error:'content missing'
         })
     }
 
-    if(phonebook.some(person => person.name ===body.name)){
-        return res.status(400).json({error:'name already exists'})
-    }
-
-    const person = {
+    const person = new Person ({
         name:body.name,
         number:body.number,
-        id:Math.floor(Math.random() * 100000000000)
-    }
-    phonebook = phonebook.concat(person)
-    res.json(person)
+    })
+
+    person.save().then(newNote => res.json(newNote))
 })
 
 app.delete('/api/persons/:id', (req,res) =>{
