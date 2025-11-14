302e2b3afa79341ef2c47a7aa1b5c336f652e28c
liav lavi <liav.lavi@gmail.com>
2025-11-14 20:20:11 +0200

    Fixing project lints v2

---
+++ part11/eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from 'globals'
+import js from '@eslint/js'
+
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+      ecmaVersion: 'latest',
+    },
+  },
+]
