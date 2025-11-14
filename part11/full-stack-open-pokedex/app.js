c847a65299aa9785f418063d40bcb4213e8e97c9
liav lavi <liav.lavi@gmail.com>
2025-11-14 20:22:37 +0200

    Fixing project lints v2

--- part11/full-stack-open-pokedex/app.js
+++ part11/full-stack-open-pokedex/app.js
@@ -1,14 +1,16 @@
- 
+/* eslint-disable no-undef */
+
 const express = require('express')
 const app = express()
 
 // get the port from env variable
- 
+
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-   
+
+  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
